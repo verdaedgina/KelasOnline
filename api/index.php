917545517fa6e78ea805a7eb759bbd7169b27e3d
verdaedgina <verdaedgina@gmail.com>
2025-03-05 10:12:25 +0700

    n

--- api/index.php
+++ api/index.php
@@ -1,3 +1,8 @@
 <?php
 
-require __DIR__ . '/../public/index.php';
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$request = Illuminate\Http\Request::capture();
+$app->make(Illuminate\Contracts\Http\Kernel::class)->handle($request)->send();
